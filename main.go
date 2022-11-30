0d9bc3949ea04b96698b246fe1806073b8e6660a
YABOI <110062350+YABOIpy@users.noreply.github.com>
2022-11-30 14:58:20 +0100

    Update main.go

--- main.go
+++ main.go
@@ -29,13 +29,18 @@ func MassDm() {
 		go func(i int) {
 			defer wg.Done()
 			for _, UserID := range ids {
+				if c.Config().Settings.Websock == true {
+					c.WebSock(strings.Join(Token, ""))							
+				}
 				ID,_ := strconv.Atoi(UserID)
-				c.Create(ID, strings.Join(Token, ""), message)
+				CID, err := c.Create(ID, strings.Join(Token, ""), message)
+				c.Errs(err)
+
 				if c.Config().Settings.Close == true {
-					//									
-				} 
+					c.CloseDm(CID, strings.Join(Token, ""), massdm.Cookies)							
+				} else if c.Config().Settings.Close == false {}
 				if c.Config().Settings.Block == true {
-					//
+					c.Block(ID, strings.Join(Token, ""), massdm.Cookies)
 				}
 			}
 		}(i)
@@ -48,7 +53,17 @@ func MassDm() {
 
 func main() {
 	logo := `
-	
+	_______________     ______________  ___
+	__  ____/_  __ \    ___  __ \__   |/  /
+	_  / __ _  / / /    __  / / /_  /|_/ / 
+	/ /_/ / / /_/ /     _  /_/ /_  /  / /  
+	\____/  \____/      /_____/ /_/  /_/   
+    
+	[Go Discord Mass Dm]		~YABOI
+	[1]
+	[2]
+	[3]
+	[4]
 	`
 	fmt.Println(logo)
 	MassDm()	
