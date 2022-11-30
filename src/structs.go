051571b44be18b4828a0eac0c0418bab8eb66661
YABOI <110062350+YABOIpy@users.noreply.github.com>
2022-11-30 05:58:05 -0800

    Add files via upload

---
+++ src/structs.go
@@ -0,0 +1,62 @@
+package massdm
+
+import (
+	"net/url"
+	"net/http"
+	"crypto/tls"
+	"github.com/gorilla/websocket"
+)
+
+
+type ChannelID struct {
+	ID 		string `json:"id,omitempty"`
+}
+
+
+
+type Config struct {
+	Headers map[string]string
+	Dcfd	string
+	Sdcfd	string
+	Length	int
+	ID  	string	
+
+	XConfig struct {
+		Proxy 	 string `json:"Proxy"`
+	}`json:"Config"`
+
+	Settings struct {
+		Websock	 bool `json:"Websocket"`
+		Block	 bool `json:"Block_Usr"`
+		Close    bool `json:"Close_DM"`
+	} `json:"Settings"`
+}
+
+type Connect struct {
+	Token         string
+	Messages      chan []byte
+	Complete      bool
+	Conn          *websocket.Conn
+	sessionID     string
+	in            chan string
+	out           chan []byte
+	fatalHandler  func(err error)
+	seq           int
+	closeChan     chan struct{}
+	Reactions     chan []byte
+}
+
+var (
+	c = X()
+	proxy = c.Config().XConfig.Proxy
+	p, _ = url.Parse("http://" + proxy)
+	Client = http.Client {
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				MaxVersion: tls.VersionTLS13,
+			},
+			Proxy: http.ProxyURL(p),
+		},
+	}
+	Cookies = "__dcfduid=" + cookies().Dcfd + "; " + "__sdcfduid=" + cookies().Sdcfd + "; "
+)
