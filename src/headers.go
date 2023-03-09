e1ce4ac0846b5f147f14806ceb27d17c1a7fee12
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-03-09 22:09:44 +0100

    Update headers.go

--- src/headers.go
+++ src/headers.go
@@ -1,8 +1,9 @@
 package massdm
 
 import (
-	http "github.com/Danny-Dasilva/fhttp"
 	"strconv"
+
+	http "github.com/Danny-Dasilva/fhttp"
 )
 
 func (Hd *Header) Head_Dm(req *http.Request, Token string) {
@@ -12,7 +13,7 @@ func (Hd *Header) Head_Dm(req *http.Request, Token string) {
 		"accept-language":    "en-US,en-GB;q=0.9",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/",
 		"sec-fetch-dest":     "empty",
@@ -33,7 +34,7 @@ func (Hd *Header) Head_CloseDm(req *http.Request, Token string) {
 		"accept-language":    "en-US,en-GB;q=0.9",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/",
 		"sec-fetch-dest":     "empty",
@@ -54,7 +55,7 @@ func (Hd *Header) Head_React(req *http.Request, Token string) {
 		"accept-language":    "en-US,en-GB;q=0.9",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/",
 		"sec-fetch-dest":     "empty",
@@ -75,7 +76,7 @@ func (Hd *Header) Head_Create(req *http.Request, Token string) {
 		"accept-language":      "en-US,en-GB;q=0.9",
 		"authorization":        Token,
 		"content-type":         "application/json",
-		"cookie":               Cookies,
+		"cookie":               c.GetCookie(),
 		"origin":               "https://discord.com",
 		"referer":              "https://discord.com/channels/",
 		"sec-fetch-dest":       "empty",
@@ -97,7 +98,7 @@ func (Hd *Header) Head_Block(req *http.Request, Token string, ID int) {
 		"accept-language":    "en-US,en-GB;q=0.9",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/@me/" + strconv.Itoa(ID) + "",
 		"sec-fetch-dest":     "empty",
@@ -118,7 +119,7 @@ func (Hd *Header) Head_Joiner(req *http.Request, Token string) {
 		"accept-language":    "en-US,en-GB;q=0.9",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/",
 		"sec-fetch-dest":     "empty",
@@ -139,7 +140,7 @@ func (Hd *Header) Head_Leaver(req *http.Request, Token string) {
 		"accept-language":    "en-US,en-GB;q=0.9",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/",
 		"sec-fetch-dest":     "empty",
@@ -161,7 +162,7 @@ func (Hd *Header) Head_Agree(req *http.Request, Token string, val int) {
 			"accept-language":    "en-US,en-GB;q=0.9",
 			"authorization":      Token,
 			"content-type":       "application/json",
-			"cookie":             Cookies,
+			"cookie":             c.GetCookie(),
 			"origin":             "https://discord.com",
 			"referer":            "https://discord.com/channels/",
 			"sec-fetch-dest":     "empty",
@@ -181,7 +182,7 @@ func (Hd *Header) Head_Agree(req *http.Request, Token string, val int) {
 			"accept-language":    "en-US,en-GB;q=0.9",
 			"authorization":      Token,
 			"content-type":       "application/json",
-			"cookie":             Cookies,
+			"cookie":             c.GetCookie(),
 			"origin":             "https://discord.com",
 			"referer":            "https://discord.com/channels/",
 			"sec-fetch-dest":     "empty",
@@ -203,7 +204,7 @@ func (Hd *Header) Head_Raider(req *http.Request, Token string, ID string) {
 		"accept-language":    "en-US,en-NL;q=0.9,en-GB;q=0.8",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/@me/" + ID + "",
 		"sec-fetch-dest":     "empty",
@@ -224,9 +225,30 @@ func (Hd *Header) Head_MassPing(req *http.Request, Token string, ID string) {
 		"accept-language":    "en-US,en-NL;q=0.9,en-GB;q=0.8",
 		"authorization":      Token,
 		"content-type":       "application/json",
-		"cookie":             Cookies,
+		"cookie":             c.GetCookie(),
 		"origin":             "https://discord.com",
 		"referer":            "https://discord.com/channels/@me/" + ID + "",
+		"sec-fetch-dest":     "empty",
+		"sec-fetch-mode":     "cors",
+		"sec-fetch-site":     "same-origin",
+		"user-agent":         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9006 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36",
+		"x-debug-options":    "bugReporterEnabled",
+		"x-discord-locale":   "en-US",
+		"x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDA3Iiwib3NfdmVyc2lvbiI6IjEwLjAuMjIwMDAiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTYyNzg4LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
+	} {
+		req.Header.Set(x, o)
+	}
+}
+func (Hd *Header) Head_Button(req *http.Request, Token string, GID string, CID string) {
+	for x, o := range map[string]string{
+		"accept":             "*/*",
+		"accept-encoding":    "gzip, deflate, br",
+		"accept-language":    "en-US,en-NL;q=0.9,en-GB;q=0.8",
+		"authorization":      Token,
+		"content-type":       "application/json",
+		"cookie":             c.GetCookie(),
+		"origin":             "https://discord.com",
+		"referer":            "https://discord.com/channels/" + GID + "/" + CID + "",
 		"sec-fetch-dest":     "empty",
 		"sec-fetch-mode":     "cors",
 		"sec-fetch-site":     "same-origin",
