685254e28dfa213330f34e453c658eb08265d30d
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-04-09 05:09:40 +0200

    Update headers.go

--- src/headers.go
+++ src/headers.go
@@ -197,6 +197,30 @@ func (Hd *Header) Head_Agree(req *http.Request, Token string, val int) {
 		}
 	}
 }
+
+func (Hd *Header) Head_Friend(req *http.Request, Token string) {
+	for x, o := range map[string]string{
+		"accept":               "*/*",
+		"accept-encoding":      "gzip, deflate, br",
+		"accept-language":      "en-US,en-NL;q=0.9,en-GB;q=0.8",
+		"authorization":        Token,
+		"content-type":         "application/json",
+		"cookie":               c.GetCookie(),
+		"origin":               "https://discord.com",
+		"referer":              "https://discord.com/channels/@me/",
+		"sec-fetch-dest":       "empty",
+		"sec-fetch-mode":       "cors",
+		"sec-fetch-site":       "same-origin",
+		"user-agent":           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9006 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36",
+		"x-context-properties": "eyJsb2NhdGlvbiI6IkFkZCBGcmllbmQifQ==", //(ױk'{"location":"Add Friend"}
+		"x-debug-options":      "bugReporterEnabled",
+		"x-discord-locale":     "en-US",
+		"x-super-properties":   "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDA3Iiwib3NfdmVyc2lvbiI6IjEwLjAuMjIwMDAiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTYyNzg4LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
+	} {
+		req.Header.Set(x, o)
+	}
+}
+
 func (Hd *Header) Head_Raider(req *http.Request, Token string, ID string) {
 	for x, o := range map[string]string{
 		"accept":             "*/*",
