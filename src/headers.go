8d5d77b09aeb012bc12ca2c17203f351136900f7
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-04-28 15:19:49 -0700

    Update headers.go

--- src/headers.go
+++ src/headers.go
@@ -114,21 +114,22 @@ func (Hd *Header) Head_Block(req *http.Request, Token string, ID int) {
 }
 func (Hd *Header) Head_Joiner(req *http.Request, Token string) {
 	for x, o := range map[string]string{
-		"accept":             "*/*",
-		"accept-encoding":    "gzip, deflate, br",
-		"accept-language":    "en-US,en-GB;q=0.9",
-		"authorization":      Token,
-		"content-type":       "application/json",
-		"cookie":             c.GetCookie(),
-		"origin":             "https://discord.com",
-		"referer":            "https://discord.com/channels/",
-		"sec-fetch-dest":     "empty",
-		"sec-fetch-mode":     "cors",
-		"sec-fetch-site":     "same-origin",
-		"user-agent":         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9007 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36",
-		"x-debug-options":    "bugReporterEnabled",
-		"x-discord-locale":   "en-US",
-		"x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDA3Iiwib3NfdmVyc2lvbiI6IjEwLjAuMjIwMDAiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTYyNjg2LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==",
+		"accept":               "*/*",
+		"accept-encoding":      "gzip, deflate, br",
+		"accept-language":      "en-US,en-GB;q=0.9",
+		"authorization":        Token,
+		"content-type":         "application/json",
+		"cookie":               c.GetCookie(),
+		"origin":               "https://discord.com",
+		"referer":              "https://discord.com/channels/",
+		"sec-fetch-dest":       "empty",
+		"sec-fetch-mode":       "cors",
+		"sec-fetch-site":       "same-origin",
+		"user-agent":           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9007 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36",
+		"x-context-properties": "eyJsb2NhdGlvbiI6Ikludml0ZSBCdXR0b24gRW1iZWQiLCJsb2NhdGlvbl9ndWlsZF9pZCI6bnVsbCwibG9jYXRpb25fY2hhbm5lbF9pZCI6IjEwOTc0NjI5MDc5OTE3NTY4MzAiLCJsb2NhdGlvbl9jaGFubmVsX3R5cGUiOjEsImxvY2F0aW9uX21lc3NhZ2VfaWQiOiIxMTAxNjIzMjYyNTQwMjIyNTI1In0=",
+		"x-debug-options":      "bugReporterEnabled",
+		"x-discord-locale":     "en-US",
+		"x-super-properties":   "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDEyIiwib3NfdmVyc2lvbiI6IjEwLjAuMjI2MjEiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTkzOTA2LCJuYXRpdmVfYnVpbGRfbnVtYmVyIjozMjAyMCwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==",
 	} {
 		req.Header.Set(x, o)
 	}
