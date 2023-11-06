9b9035ced204794cec5ff6714a9bab9e989a91f3
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-11-06 11:14:41 +0100

    Update websocket.go

--- src/modules/websocket.go
+++ src/modules/websocket.go
@@ -1,8 +1,6 @@
 package modules
 
 import (
-	crand "crypto/rand"
-	"encoding/base64"
 	"encoding/json"
 	"github.com/gorilla/websocket"
 	"log"
@@ -11,7 +9,7 @@ import (
 	"time"
 )
 
-func (ws *Sock) Connect(Token string, in Instance) (*WsResp, []byte, *Sock) {
+func (ws *Sock) Connect(Token string, in *Instance) (*WsResp, *Sock, error) {
 	var dialer websocket.Dialer
 
 	conn, _, err := dialer.Dial(
@@ -23,78 +21,70 @@ func (ws *Sock) Connect(Token string, in Instance) (*WsResp, []byte, *Sock) {
 			"Pragma":                {"no-cache"},
 			"Sec-WebSocket-Version": {"13"},
 			"User-Agent":            {in.BrowserClient.Agent},
-			//"Sec-WebSocket-Key":     {ws.WsKey()},
-
 		},
 	)
 	if err != nil {
 		log.Println(err)
-		if len(Token) != 0 {
+		if len(Token) != IntNil {
 			return websock.Connect(Token, in)
 		}
 	}
+	conn.ReadMessage()
 
-	ws.Ws = conn
-	d, b, c := ws.Send(conn, Token, in)
+	d, c, err := send(conn, Token, in)
+	if err != nil {
+		return nil, nil, err
+	}
 
-	return d, b, &Sock{Ws: c}
+	return d, &Sock{Ws: c}, nil
 }
 
-func (c *Sock) ScrapeUsers(GID string, CID string, iter int) []Member {
+func (ws *Sock) ScrapeUsers(GID, CID string, iter int) []Member {
 
-	time.Sleep(1 * time.Second)
-	c.GuildConnection(c.Ws, GID, CID, iter)
+	time.Sleep(time.Second)
+	ws.GuildConnection(GID, CID, iter)
 
-	_, b, _ := c.Ws.ReadMessage()
+	_, b, _ := ws.Ws.ReadMessage()
 	var data WsResp
 	json.Unmarshal(b, &data)
 
 	if data.Name == EventGuildMemberListUpdate {
 		for i := 0; i < len(data.Data.Ops); i++ {
-			if len(data.Data.Ops[i].Items) == 0 && data.Data.Ops[i].Op == EventSync {
-				c.Break = true
+			if len(data.Data.Ops[i].Items) == IntNil && data.Data.Ops[i].Op == EventSync {
+				ws.Break = true
 			}
 		}
 		for i := 0; i < len(data.Data.Ops); i++ {
 			if data.Data.Ops[i].Op == EventSync {
 				for j := 0; j < len(data.Data.Ops[i].Items); j++ {
-					c.Members = append(c.Members, data.Data.Ops[i].Items[j].Member)
+					ws.Members = append(ws.Members, data.Data.Ops[i].Items[j].Member)
 				}
 			}
 		}
 	}
-
-	return c.Members
+	return ws.Members
 }
 
-func (s *Sock) GuildConnection(Ws *websocket.Conn, GID string, CID string, iter int) {
-	if iter == 0 {
-		s.GuildCon(Ws, GID, CID)
+func (ws *Sock) GuildConnection(GID, CID string, iter int) {
+	if iter == IntNil {
+		ws.GuildCon(GID, CID)
 	}
 
-	var v []interface{}
-	switch iter {
-	case 0:
-		v = []interface{}{[2]int{0, 99}}
-	case 1:
-		v = []interface{}{[2]int{0, 99}, [2]int{100, 199}}
-	default:
-		v = []interface{}{[2]int{0, 99}, [2]int{100, 199}, [2]int{iter * 100, (iter * 100) + 99}}
-	}
-
-	Ws.WriteJSON(WsResp{
+	if err := ws.Ws.WriteJSON(WsResp{
 		Op: 14,
 		Data: WsData{
 			GuildId: GID,
 			Channels: map[string]interface{}{
-				CID: v,
+				CID: CreateRange(iter),
 			},
 		},
-	})
+	}); err != nil {
+		log.Println(err)
+	}
 }
 
-func (s *Sock) GuildCon(Ws *websocket.Conn, GID string, CID string) {
-	Ws.WriteJSON(WsResp{
+func (ws *Sock) GuildCon(GID, CID string) {
+	if err := ws.Ws.WriteJSON(WsResp{
 		Op: 14,
 		Data: WsData{
 			GuildId:    GID,
@@ -106,70 +96,66 @@ func (s *Sock) GuildCon(Ws *websocket.Conn, GID string, CID string) {
 				CID: []interface{}{[2]int{0, 99}},
 			},
 		},
-	})
-}
-
-func (ws *Sock) WsKey() string {
-	B := make([]byte, 16)
-	_, err := crand.Read(B)
-	if err != nil {
+	}); err != nil {
 		log.Println(err)
-		return ""
 	}
-	return base64.StdEncoding.EncodeToString(B)
 }
 
-func (c *Sock) Send(conn *websocket.Conn, Token string, in Instance) (*WsResp, []byte, *websocket.Conn) {
-	_, d, _ := conn.ReadMessage()
-	var resp WsResp
-	json.Unmarshal(d, &resp)
-	conn.WriteJSON(map[string]interface{}{
-		"op": 2,
-		"d": map[string]interface{}{
-			"token":        Token,
-			"capabilities": 125,
-			"properties": map[string]interface{}{
-				"os":                       in.BrowserClient.OS,
-				"browser":                  in.BrowserClient.Name,
-				"system_locale":            "en-US",
-				"browser_user_agent":       in.BrowserClient.Agent,
-				"browser_version":          in.BrowserClient.Version,
-				"os_version":               in.BrowserClient.OSVer,
-				"referrer":                 "https://www.google.com",
-				"referring_domain":         "www.google.com",
-				"referrer_current":         "",
-				"referring_domain_current": "",
-				"release_channel":          "stable",
-				"client_build_number":      cbn,
+func send(conn *websocket.Conn, Token string, in *Instance) (*WsResp, *websocket.Conn, error) {
+	err := conn.WriteJSON(WsResp{
+		Op: 2,
+		Data: WsData{
+			Token:        Token,
+			Capabilities: capabilities,
+			Compress:     false,
+			Properties: XpropData{
+				OS:                 in.BrowserClient.OS,
+				Browser:            in.BrowserClient.Name,
+				SystemLocale:       "en-US",
+				BrowserUserAgent:   in.BrowserClient.Agent,
+				OSVersion:          in.BrowserClient.OSVer,
+				Referrer:           "https://www.google.com",
+				ReferringDomain:    "www.google.com",
+				ReferrerCurrent:    "",
+				ReferringDomainCur: "",
+				Device:             "",
+				ReleaseChannel:     "stable",
+				ClientBuildNumber:  cbn,
+				ClientEventSource:  nil,
 			},
-			"presence": map[string]interface{}{
-				"status": in.Cfg.Mode.Discord.Presence[rand.Intn(len(in.Cfg.Mode.Discord.Presence))],
-				"game": map[string]interface{}{
-					"name": "GoDm - github.com/yaboipy/godm",
-					"type": 0,
+			Presence: Presence{
+				Status:     in.Cfg.Mode.Discord.Presence[rand.Intn(len(in.Cfg.Mode.Discord.Presence))],
+				Activities: []string{},
+				Afk:        false,
+				Game: Game{
+					Name: "GoDm - github.com/yaboipy/godm",
+					Type: 0,
 				},
-				"since":      0,
-				"activities": []string{},
-				"afk":        false,
 			},
-			"compress": false,
-			"client_state": map[string]interface{}{
-				"highest_last_message_id":     "0",
-				"read_state_version":          0,
-				"user_guild_settings_version": -1,
-				"user_settings_version":       -1,
+			ClientState: ClientState{
+				GuildVersions:            struct{}{},
+				HighestLastMessageID:     "0",
+				PrivateChannelsVersion:   "0",
+				ReadStateVersion:         0,
+				UserGuildSettingsVersion: -1,
+				UserSettingsVersion:      -1,
+				ApiCodeVersion:           0,
 			},
 		},
 	})
+	if err != nil {
+		log.Println(err)
+	}
 
 	_, b, err := conn.ReadMessage()
 	if err != nil {
 		log.Println(err)
-		return nil, nil, nil //return c.Send(conn, Token, in)
+		return nil, nil, err
 	}
 	var data WsResp
-	json.Unmarshal(b, &data)
-	data.Data.HeartbeatInterval = resp.Data.HeartbeatInterval
+	if err = json.Unmarshal(b, &data); err != nil {
+		log.Println(err)
+	}
 
-	return &data, b, conn
+	return &data, conn, nil
 }
